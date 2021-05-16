ce32c85708d951bc88de0b8c01f6937dd87af888
Nokimochi1 <75022556+Nokimochi1@users.noreply.github.com>
2021-05-16 18:58:19 +0200

    Update token.sol

--- token.sol
+++ token.sol
@@ -243,7 +243,46 @@ contract Staking is Token{
     }
 }
 
-contract Referals is Token{
+contract Referrals is Token{
 
+    event ReferralsWithdraw(address to, uint amount);
+    
+    struct RefsInfo{
+        address[] peopleRefered;
+        uint amountOfPeopleRefered;
+        uint bountyTaken;
+    }
 
+    mapping(address => RefsInfo) ReferalsInformation;
+
+    function checkRef (address ad) private view returns(bool){
+        for (uint i = 0; i<ReferalsInformation[msg.sender].amountOfPeopleRefered; i++){
+            if (ReferalsInformation[msg.sender].peopleRefered[i] == ad){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function addRef(address referedAddress) public returns(bool){
+        require(checkRef(referedAddress) == true, "sorry you already did invite that person");
+        ReferalsInformation[msg.sender].peopleRefered.push(referedAddress);
+        ReferalsInformation[msg.sender].amountOfPeopleRefered += 1;
+        ReferalsInformation[msg.sender].bountyTaken += 100;
+        return true;
+    }
+
+    function withdrawReferral(uint amount) public returns(bool){
+        require(ReferalsInformation[msg.sender].bountyTaken >= amount);
+        ReferalsInformation[msg.sender].bountyTaken -= amount;
+        balances[msg.sender] += amount;
+        emit ReferralsWithdraw(msg.sender, amount);
+        return true;
+    }
+
+}
+
+contract Gambling is Token {
+
+    
 }
